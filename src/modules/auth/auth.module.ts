96002abb284c6f8342b12ad0cd5cc0ccb75242cb
Shalon N. Ngigi <njery.ngigi@gmail.com>
2024-09-26 02:35:23 +0300

    migrate to the jwt provider

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,22 +1,13 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 import { LoginValidatorMiddleware } from '../../middlewares/login.middleware';
 import { SignupValidatorMiddleware } from '../../middlewares/signup.middleware';
+import { JwtModule } from '../jwt/jwt.module';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JWT_SECRET } from './constants';
 
 @Module({
-  imports: [
-    UserModule,
-    JwtModule.register({
-      global: true,
-      secret: JWT_SECRET,
-      // TODO: Verify right way to set expiresIn and how to issue a refresh token
-      signOptions: { expiresIn: '60s' },
-    }),
-  ],
+  imports: [UserModule, JwtModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,24 +1,17 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { AuthDto } from '../dto/auth.dto';
 import { LoginDto } from '../dto/login.dto';
 import { SignupDto } from '../dto/signup.dto';
-import { User } from '../user/user.model';
+import { JwtProvider } from '../jwt/jwt.provider';
 import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private userService: UserService,
-    private jwtService: JwtService,
+    private jwtService: JwtProvider,
   ) {}
-
-  async generateAccessToken(user: User): Promise<AuthDto> {
-    const payload = { email: user.email, id: user.id };
-    const accessToken = await this.jwtService.signAsync(payload);
-    return { accessToken };
-  }
 
   async loginUser(user: LoginDto): Promise<AuthDto> {
     const dbUser = await this.userService.findOneByEmail(user.email);
@@ -35,11 +28,11 @@ export class AuthService {
       throw new UnauthorizedException('Invalid email or password');
     }
 
-    return this.generateAccessToken(dbUser);
+    return await this.jwtService.generateAccessToken(dbUser);
   }
 
   async signupUser(user: SignupDto): Promise<AuthDto> {
     const dbUser = await this.userService.createUser(user);
-    return this.generateAccessToken(dbUser);
+    return await this.jwtService.generateAccessToken(dbUser);
   }
 }
