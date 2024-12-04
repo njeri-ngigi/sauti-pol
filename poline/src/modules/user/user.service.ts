00fbd372efd326262496d46912d20ff2aa49e686
Shalon N. Ngigi <njery.ngigi@gmail.com>
2024-12-04 20:13:37 +0300

    add logging

--- poline/src/modules/user/user.service.ts
+++ poline/src/modules/user/user.service.ts
@@ -3,6 +3,7 @@ import {
   ConflictException,
   Inject,
   Injectable,
+  Logger,
   NotFoundException,
 } from '@nestjs/common';
 import { Op } from 'sequelize';
@@ -14,20 +15,27 @@ import { User } from './user.model';
 
 @Injectable()
 export class UserService {
+  private readonly logger = new Logger(UserService.name, {
+    timestamp: true,
+  });
+
   constructor(
     @Inject(USER_REPOSITORY) private readonly userModel: typeof User,
   ) {}
 
   async findOneById(id: string): Promise<User> {
     if (!isUUID(id)) {
+      this.logger.error('Invalid UUID');
       throw new BadRequestException('Invalid UUID');
     }
 
     const user = await this.userModel.findByPk<User>(id);
     if (!user) {
+      this.logger.error('User not found');
       throw new NotFoundException('User not found');
     }
 
+    this.logger.debug(`User ${user.email} found`);
     return user;
   }
 
@@ -52,15 +60,18 @@ export class UserService {
     });
 
     if (dbUserByEmailOrPhone) {
+      this.logger.error('User with email or phone already exists');
       throw new ConflictException('User with email or phone already exists');
     }
 
+    this.logger.debug(`Creating user ${user.email}`);
     return this.userModel.create<User>(user);
   }
 
   async updateUserRole(id: string, role: Role): Promise<User> {
     const dbUser = await this.findOneById(id);
 
+    this.logger.debug(`Updating role for user ${dbUser.email} to ${role}`);
     return dbUser.update({ role });
   }
 
