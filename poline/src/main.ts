a9eafd9e704d9f0ac872853b41d1f55b1e86c65a
Shalon N. Ngigi <njery.ngigi@gmail.com>
2024-12-04 20:09:06 +0300

    parse cookies; enable cors

--- poline/src/main.ts
+++ poline/src/main.ts
@@ -1,15 +1,38 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug'],
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Poline API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
+  app.enableCors({
+    origin: 'http://localhost:8080', // Frontend URL
+    credentials: true, // Allow cookies to be sent
+  });
+
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+  app.use(cookieParser());
+
   await app.listen(3000);
+
+  console.log(
+    `Application is running in: ${process.env.NODE_ENV} on ${await app.getUrl()}`,
+  );
 }
 bootstrap();
